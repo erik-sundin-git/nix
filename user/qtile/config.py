4d51e7ae01fa8a8db238276d26b1c7508cfd53c7
Erik <asd@asd.se>
2024-05-21 19:38:46 +0200

    qtile

--- user/qtile/config.py
+++ user/qtile/config.py
@@ -1,6 +1,6 @@
 # Erik Sundin
 # Qtile config
-# 
+#
 # colorpalette: https://coolors.co/d1f0b1-b6cb9e-92b4a7-8c8a93-81667a
 
 from libqtile import bar, layout, qtile, widget, hook
@@ -16,62 +16,80 @@ terminal = "alacritty"
 browser = "qutebrowser"
 menu = "rofi -show drun"
 FONT_SIZE = 14
-host_name = socket.gethostname()
 
 desktop = "nixos"
 
+
 @hook.subscribe.startup_once
 def start_once():
-    home = os.path.expanduser('~')
-    subprocess.call([home + '/nix/user/qtile/autostart.sh'])
+    home = os.path.expanduser("~")
+    subprocess.call([home + "/nix/user/qtile/autostart.sh"])
+
+
+def get_vendor_info():
+    path = "/sys/class/dmi/id/sys_vendor"
+    try:
+        with open(path, "r") as file:
+            vendor_info = file.read().strip()
+    except FileNotFoundError:
+        vendor_info = "Unknown"
+    except PermissionError:
+        vendor_info = "Permission Denied"
+
+    return vendor_info
+
+
+host_name = get_vendor_info()
 
 
 def battery_widget():
-    widget.Battery(
-            background = "B6CB9E",
-            low_foreground = "000000", 
-            low_background = "ff0010",
-            format = "{char}{percent: 2.0%}",
-            foreground = "000000",
-            low_percentage = 0.15)
+    w = widget.Battery(
+        background="B6CB9E",
+        low_foreground="000000",
+        low_background="ff0010",
+        format="{char}{percent: 2.0%}",
+        foreground="000000",
+        low_percentage=0.15,
+    )
+    return w
+
 
 def create_bars() -> bar.Bar:
-    '''
+    """
     Create bar
-    '''
+    """
     newBar = bar.Bar(
-                        [
-                widget.Spacer(10),
-                widget.CurrentLayout(fontsize=FONT_SIZE),
-                widget.Spacer(),
-                widget.GroupBox(fontsize=FONT_SIZE),
-                widget.Prompt(),
-                widget.Spacer(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.PulseVolume(),
-                widget.Bluetooth(
-                    background = "92B4A7"
-                    ),
-                #widget.TextBox("eriks", name="custom"),
-                widget.Memory(
-                    background = "B6CB9E"
-                ),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %H:%M %p" ),
-                widget.QuickExit(),
-            ],
-            30,
-            background="141414",
-            border_width=[2, 0, 2, 0],
-            border_color=["000000", "000000", "000000", "000000"]
-        )
+        [
+            widget.Spacer(10),
+            widget.CurrentLayout(fontsize=FONT_SIZE),
+            widget.Spacer(),
+            widget.GroupBox(fontsize=FONT_SIZE),
+            widget.Prompt(),
+            widget.Spacer(),
+            widget.Chord(
+                chords_colors={
+                    "launch": ("#ff0000", "#ffffff"),
+                },
+                name_transform=lambda name: name.upper(),
+            ),
+            battery_widget() if host_name == "LENOVO" else widget.Spacer(),
+            widget.PulseVolume(),
+            widget.Bluetooth(background="92B4A7"),
+            # widget.TextBox("eriks", name="custom"),
+            widget.Memory(background="B6CB9E"),
+            widget.Systray(),
+            widget.Clock(format="%Y-%m-%d %a %H:%M %p"),
+            widget.QuickExit(),
+        ],
+        30,
+        background="141414",
+        border_width=[2, 0, 2, 0],
+        border_color=["000000", "000000", "000000", "000000"],
+    )
     return newBar
-#END create_bars()
+
+
+# END create_bars()
 
 keys = [
     # Switch between windows
@@ -82,14 +100,23 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -103,7 +130,7 @@ keys = [
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([mod], "Return", lazy.spawn(terminal+" -e fish"), desc="Launch terminal"),
+    Key([mod], "Return", lazy.spawn(terminal + " -e fish"), desc="Launch terminal"),
     Key([mod], "d", lazy.spawn(menu), desc="Launch menu"),
     Key([mod, "shift"], "f", lazy.spawn(browser), desc="Launch browser"),
     # Toggle between different layouts as defined below
@@ -115,36 +142,32 @@ keys = [
         lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key(
+        [mod],
+        "t",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window",
+    ),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
-
     # screenstuff
-
     Key(
-        [mod, "shift"], "Right",  # Example key combination (Ctrl + Left Arrow)
+        [mod, "shift"],
+        "Right",  # Example key combination (Ctrl + Left Arrow)
         lazy.window.toscreen(1),  # Move window to screen 1
         lazy.to_screen(1),
-        desc="Move window to screen 1"
+        desc="Move window to screen 1",
     ),
     Key(
-        [mod, "shift"], "Left",  # Example key combination (Ctrl + Right Arrow)
+        [mod, "shift"],
+        "Left",  # Example key combination (Ctrl + Right Arrow)
         lazy.window.toscreen(0),  # Move window to screen 0
         lazy.to_screen(0),
-        desc="Move window to screen 0"
+        desc="Move window to screen 0",
     ),
-    Key(
-        [mod], "right",
-        lazy.to_screen(1),
-        desc="Move focus to next monitor"
-    ),
-    Key(
-        [mod], "left",
-        lazy.to_screen(0),
-        desc="Move focus to previous monitor"
-    ),
+    Key([mod], "right", lazy.to_screen(1), desc="Move focus to next monitor"),
+    Key([mod], "left", lazy.to_screen(0), desc="Move focus to previous monitor"),
 ]
 
 # Add key bindings to switch VTs in Wayland.
@@ -161,15 +184,17 @@ for vt in range(1, 8):
     )
 
 
-groups = [Group("1"), 
-          Group("2"), 
-          Group("3"), 
-          Group("4", matches=Match(wm_class="Bitwarden"), label="util"), 
-          Group("5", matches=Match(wm_class="pavucontrol"), label="musik"), 
-          Group("6", matches=Match(wm_class="virt-manager"), label="vm"), 
-          Group("7"), 
-          Group("8"), 
-          Group("9")]
+groups = [
+    Group("1"),
+    Group("2"),
+    Group("3"),
+    Group("4", matches=Match(wm_class="Bitwarden"), label="util"),
+    Group("5", matches=Match(wm_class="pavucontrol"), label="musik"),
+    Group("6", matches=Match(wm_class="virt-manager"), label="vm"),
+    Group("7"),
+    Group("8"),
+    Group("9"),
+]
 
 for i in groups:
     keys.extend(
@@ -187,10 +212,14 @@ for i in groups:
             #   i.name,
             #    lazy.window.togroup(i.name, switch_group=True),
             #   desc="Switch to & move focused window to group {}".format(i.name),
-            #),
+            # ),
             # # mod1 + shift + group number = move focused window to group
-            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-                 desc="move focused window to group {}".format(i.name)),
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name),
+                desc="move focused window to group {}".format(i.name),
+            ),
         ]
     )
 
@@ -203,7 +232,6 @@ layouts = [
     # layout.Stack(num_stacks=2),
     # layout.Bsp
     layout.Matrix(),
-    
     # layout.RatioTile(),
     # layout.Tile(),
     # layout.TreeTab(),
@@ -218,10 +246,10 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
-    
+
 screens = [
     Screen(
-        top= create_bars(),
+        top=create_bars(),
     ),
     Screen(
         top=bar.Bar(
@@ -236,7 +264,6 @@ screens = [
 ]
 
 floating_layout = layout.Floating(
-        
     border_focus="28464B",
     float_rules=[
         *layout.Floating.default_float_rules,
@@ -246,10 +273,8 @@ floating_layout = layout.Floating(
         Match(wm_class="ssh-askpass"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
-    ]
+    ],
 )
-
-
 
 
 auto_fullscreen = True
