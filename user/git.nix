c0aea2d08c6d986377f05ef54e7005dd2a3e50a2
Erik <asd@asd.se>
2024-06-11 08:18:28 +0200

    refactoring and cleanup

---
+++ user/git.nix
@@ -0,0 +1,10 @@
+{
+  programs.git = {
+    enable = true;
+    userName = "Erik Sundin";
+    userEmail = "mail@eriksundin.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+}
--- user/home.nix
+++ user/home.nix
@@ -1,59 +1,27 @@
- # home-manager
- #
- # Erik Sundin
- #
-
-{ config, pkgs, ... }:
-
+# home-manager
+#
+# Erik Sundin
+#
 {
-    imports =
-    [
-      ./picom.nix
- #     ./hyprland.nix
- #     ./waybar.nix
-      ./terminal/alacritty.nix
- #     ./terminal/vim.nix
-      ./terminal/neovim.nix
-	  ./terminal/fish.nix
-	  ./rofi.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./picom.nix
+    ./terminal/alacritty.nix
+    ./git.nix
+    ./vscode.nix
+    ./terminal/neovim.nix
+    ./terminal/fish.nix
+    ./rofi.nix
+  ];
 
   # manage.
   home.username = "erik";
   home.homeDirectory = "/home/erik";
-  
+
   nixpkgs.config.allowUnfree = true;
-  
-  # git 
-  programs.git = {
-   enable = true;
-   userName = "Erik Sundin";
-   userEmail = "mail@eriksundin.com";
-   extraConfig = {
-      init.defaultBranch = "main";
-    };
-  };
-
-  # vscode
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs; [
-      vscode-extensions.vscodevim.vim
-      vscode-extensions.jdinhlife.gruvbox
-      vscode-extensions.bbenoist.nix
-      vscode-extensions.ms-vscode.cpptools
-
-    ];
-  };
-
-
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-     hm = "home-manager switch --file ~/nix/user/home.nix";
-    };
-  };
-
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -66,24 +34,6 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -118,8 +68,6 @@
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
-
-
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
