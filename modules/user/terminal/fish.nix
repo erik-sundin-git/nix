b3685aba90de9214ba7b41d5273762f3390f8e59
Erik <asd@asd.se>
2024-07-12 21:40:40 +0200

    fish aliases for git

--- modules/user/terminal/fish.nix
+++ modules/user/terminal/fish.nix
@@ -10,16 +10,12 @@
     end\n
     funcsave fish_greeting";
     shellAliases = {
-      hm = "home-manager switch --file ~/nix/user/home.nix";
-      nx = "sudo nixos-rebuild switch --flake ~/nix/#erik";
-      nix-rebuild = "hm && nx"; #TODO maybe switch to module hm.
 
-      # ssh aliases
-      ssh-desktop = "ssh erik@192.168.1.171";
-      ssh-plex = "ssh erik@192.168.1.145";
 
       # git
       commit = "git commit -m";
+      status = "git status";
+      add = "git add";
       push = "git push origin";
     };
   };
