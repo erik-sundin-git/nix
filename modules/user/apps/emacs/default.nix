f691f742d55d368f8a0ed7f5f3fe64977f471d7c
Erik <asd@asd.se>
2024-07-27 13:10:59 +0200

    Added dotfiles for doom emacs

--- modules/user/apps/emacs/default.nix
+++ modules/user/apps/emacs/default.nix
@@ -8,6 +8,10 @@
     emacs
   ];
 
+  home.file = {
+    "/home/erik/.config/doom".source = ./doom;
+  };
+
   home.sessionPath = [
     "${config.home.homeDirectory}/.config/emacs/bin"
   ];
---
+++ modules/user/apps/emacs/doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
+ '(org-agenda-files
+   '("~/org/roam/20240727114150-hetnzer_vps.org" "/home/erik/org/roam/20240726215522-nix_config.org" "/home/erik/Documents/todo.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
